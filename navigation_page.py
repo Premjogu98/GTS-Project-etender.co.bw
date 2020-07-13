c0c186822a3fe1b39a76b7a3d55c06a9fe2a7261
premjogu007 <47113551+premjogu007@users.noreply.github.com>
2020-07-13 14:20:34 +0530

    first commit

---
+++ navigation_page.py
@@ -0,0 +1,13 @@
+
+from selenium import webdriver
+import time
+import global_var
+import html
+# from Scraping_Things import Scrap_data
+import sys
+import os
+import wx
+app = wx.App()
+
+
+def ChromeDriver():
